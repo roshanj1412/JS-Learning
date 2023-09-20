aa11b7a847d59be6019cadc06bd109a5de9b9a52
roshanj1412 <roshanjaiswal1412@gmail.com>
2023-09-20 08:30:28 +0530

    update
     update

---
+++ 01_variable.js
@@ -0,0 +1,24 @@
+var firstName="Roshan";//Variable declaration with initialization
+var lastName="Jaiswal";
+ 
+console.log(firstName);
+
+console.log(lastName);
+
+var firstName="Mr Roshan";
+
+console.log(firstName);
+
+ var height;//Variable Declaration
+ console.log(height);
+ height="6";//Variable Initialization
+
+
+console.log(height);
+
+height="6.5 feet"//Update
+console.log(height);
+
+var marks='81.80%';
+
+console.log('10th marks',marks);
---
+++ 01_variableAssign.js
@@ -0,0 +1,12 @@
+var myFullName;
+console.log(myFullName);
+myFullName="Roshan Jaiswal";
+console.log(myFullName);
+myFullName="Roshan Vijay Jaiswal";
+console.log(myFullName);
+
+var firstName="Roshan";
+console.log('My First Name:',firstName);
+
+var lastName="Jaiswal";
+console.log("My Last Name:",lastName);
