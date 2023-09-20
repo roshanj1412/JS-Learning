114c18265b870ba4d61e7d14c9c43c52995ee411
roshanj1412 <roshanjaiswal1412@gmail.com>
2023-09-20 08:59:44 +0530

    Update 01_variableAssign.js
    Assignment on Variable

--- 01_variableAssign.js
+++ 01_variableAssign.js
@@ -10,3 +10,19 @@ console.log('My First Name:',firstName);
 
 var lastName="Jaiswal";
 console.log("My Last Name:",lastName);
+
+var age="24 Years"
+console.log("Age:",age);
+
+var address="At Post sonwad bk Tal-Dharangoan Dist-Jslgoan Maharashtra";
+
+console.log("My Address:",address);
+
+var pinCode="425104"
+console.log("PinCode:",pinCode);
+
+var gender="Male";
+console.log("Male:",gender);
+
+var isMarried="No"
+console.log("Married Status:",isMarried);
