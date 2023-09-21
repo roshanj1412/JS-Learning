21e4b9d8357f2194d354ecc0b94ccc1d725672c3
roshanj1412 <roshanjaiswal1412@gmail.com>
2023-09-21 11:11:59 +0530

    02_typeOf.js

---
+++ 02_typeOf.js
@@ -0,0 +1,32 @@
+console.log("-------------Assignment 2-----------------");
+console.log("------------Step1----------------");
+var bankName;
+console.log("Bank Name:",bankName,typeof bankName);
+
+bankName="SBI Bank"
+console.log("Bank Name:",bankName,typeof bankName);
+
+console.log("------------Step2----------------");
+
+var marks='90%'
+console.log("Marks:",marks,typeof marks);
+
+console.log("------------Step3----------------");
+
+var isWorking;
+console.log("IsWorking:",isWorking,typeof isWorking);
+
+isWorking=true;
+console.log("IsWorking:",isWorking,typeof isWorking);
+
+console.log("------------Step3----------------");
+console.log("------------(Part A)----------------");
+
+var totalCount="Hundred and Seven";
+console.log("Total Count:",totalCount,typeof totalCount);
+
+console.log("------------(Part B)----------------");
+
+totalCount=107;
+console.log("Total Count:",totalCount,typeof totalCount);
+console.log("********************************************************");
