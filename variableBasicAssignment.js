0f4c179c3da3cc1834cd3a71a5e60c319681b573
roshanj1412 <roshanjaiswal1412@gmail.com>
2023-09-21 10:56:23 +0530

    variableBasiAssignment

---
+++ variableBasicAssignment.js
@@ -0,0 +1,47 @@
+console.log('----------Assignment 01  --------------');
+console.log("-----------(1st Part)---------------");
+console.log('-------number variable----------------');
+
+var pinCode=425104;
+console.log("PIN Code Number:",pinCode,typeof pinCode);
+
+var moNo=7083597475;
+console.log("Mobile Number:",moNo,typeof moNo);
+
+var salary=50000;
+console.log("Salary:",salary,typeof salary);
+
+console.log("-----------String variable-----------");
+
+var firstName="Roshan";
+console.log("First Name:",firstName,typeof firstName);
+
+var lastName="Jaiswal";
+console.log("Last Name:",lastName,typeof lastName);
+
+var fullName="Jaiswal Roshan Vijay";
+console.log("Full Name:",fullName,typeof fullName);
+
+console.log("--------Boolean Variable-----------");
+
+var areYouStudent=true;
+console.log("Are You Student:",areYouStudent,typeof areYouStudent);
+
+var areYouUnEmployed=true;
+console.log("Are you UnEmployed:",areYouUnEmployed,typeof areYouUnEmployed);
+
+var isMarried=false;
+console.log("Are you married:",isMarried,typeof isMarried);
+
+console.log("-------------(2nd Part)---------------------");
+console.log("********************Deference Between Null And Undefined********************");
+console.log("------Undefined Variable------");
+var number;
+console.log("Number:",number,typeof number);
+
+console.log("-----------Variable with null value----");
+
+number=null;
+
+console.log("Number:",number,typeof number);
+
