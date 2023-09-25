32c16a41c079bcc0e37b3e14321aaab786333afc
roshanj1412 <roshanjaiswal1412@gmail.com>
2023-09-25 13:55:04 +0530

    assignment

---
+++ 03_stringBasicassign01.js
@@ -0,0 +1,22 @@
+function stringBasic(){
+    console.log("My Dream Company is American Express(AMEX)");
+    console.log("====================================================");
+    var hobby1="Cycling";
+    var hobby2="Swimming";
+    var hobby3="Play Chess";
+    
+    console.log("Hobby1:",hobby1);
+    console.log("Hobby2:",hobby2);
+    console.log("Hobby3:",hobby3);
+
+   var  hobby1Length=hobby1.length;
+    //console.log(hobby1Lenght);
+   var  hobby2Length=hobby2.length;
+   var  hobby3Length=hobby3.length;
+
+    var totalLength=hobby1Length+hobby2Length+hobby3Length
+
+    console.log("====================================================");
+    console.log("Total Characters in Hobbies:",totalLength );
+}
+stringBasic();
