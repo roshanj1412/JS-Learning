429d79c2b63689b349d2f3fe536d4a7168a2e677
roshanj1412 <roshanjaiswal1412@gmail.com>
2023-09-25 13:36:04 +0530

    Create 03_funAssign02.js

---
+++ 03_funAssign02.js
@@ -0,0 +1,10 @@
+function bankDetails(bankName,accountNumber,location,pinCode){
+    console.log("Bank Name:",bankName,"Account No:",accountNumber,"Location:",location,"Pin Code:",pinCode);
+}
+console.log("=============Assignment 1========================");
+console.log("=============Bank Details========================");
+
+bankDetails("CITI Bank",3456782345,'Pune',431202);
+bankDetails("Axis Bank",7856782345,'Mumbai',441202);
+bankDetails("HDFC Bank",8956782345,'Pune',631202);
+
