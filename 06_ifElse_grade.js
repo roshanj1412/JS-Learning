53c859b9ffb6bd39d70dc80bf346f84a015d1eb6
roshanj1412 <roshanjaiswal1412@gmail.com>
2023-09-30 22:06:45 +0530

    Update 06_ifElse_grade.js

---
+++ 06_ifElse_grade.js
@@ -0,0 +1,31 @@
+console.log(`============Assignment 2===================`);
+var voteEligible = function(age){
+    if (age<=0 || age>130) {
+        console.log(`Invalid data `);
+    } else {
+        if (age<18) {
+            console.log(`Sorry You are not eligible for vote .`);
+        } else {
+            console.log(`You are eligible for vote , Best of Luck......`);
+        }
+    }
+}
+voteEligible(45);
+voteEligible(17);
+voteEligible(8);
+voteEligible(20);
+voteEligible(-10);
+voteEligible(200);
+voteEligible(0);
+voteEligible(null);
+voteEligible(undefined);
+
+
+
+
+
+
+
+
+
+
